09e816f80a2c6cac25c7a6b14ffedf170e7bca64
cyy <cyyever@outlook.com>
2022-12-29 10:52:13 +0800

    fix code

---
+++ cyy_ml_if/hydra/base_hook.py
@@ -0,0 +1,67 @@
+import torch
+from cyy_naive_lib.log import get_logger
+from cyy_torch_algorithm.computation.batch_hvp.batch_hvp_hook import \
+    BatchHVPHook
+from cyy_torch_algorithm.computation.sample_gradient.sample_gradient_hook import \
+    SampleGradientHook
+from cyy_torch_toolbox.device import get_device
+from cyy_torch_toolbox.hook import Hook
+
+
+class BaseHook(Hook):
+    def __init__(self, add_index=True, use_hessian=False):
+        super().__init__(stripable=True)
+        self._use_hessian = use_hessian
+        self._batch_hvp_hook = None
+        if self._use_hessian:
+            self._batch_hvp_hook = BatchHVPHook()
+
+        self._sample_gradient_hook = SampleGradientHook(add_index=add_index)
+        self._computed_indices = None
+        self._contributions: torch.Tensor | None = None
+        self._training_set_size = None
+
+    @property
+    def contributions(self):
+        return self._contributions
+
+    def _before_execute(self, **kwargs):
+        if "model_executor" in kwargs:
+            model_executor = kwargs["model_executor"]
+            trainer = model_executor
+            self._training_set_size = trainer.dataset_size
+            device = trainer.device
+        else:
+            self._training_set_size = kwargs["training_set_size"]
+            device = get_device()
+
+        if not self._computed_indices:
+            self._computed_indices = set(range(self._training_set_size))
+        else:
+            get_logger().info("only compute %s indices", len(self._computed_indices))
+        self._contributions = torch.zeros(self._training_set_size).to(
+            device, non_blocking=True
+        )
+
+    def _get_optimizer(self, **kwargs):
+        if "model_executor" in kwargs:
+            trainer = kwargs["model_executor"]
+            return trainer.get_optimizer()
+        return kwargs["optimizer"]
+
+    def _get_model_util(self, **kwargs):
+        if "model_executor" in kwargs:
+            trainer = kwargs["model_executor"]
+            return trainer.model_util
+        return kwargs["model_with_loss"].model_util
+
+    def set_computed_indices(self, computed_indices):
+        self._computed_indices = set(computed_indices)
+        self._sample_gradient_hook.set_computed_indices(computed_indices)
+
+    def _after_execute(self, **kwargs):
+        assert self._contributions is not None
+        assert self._contributions.shape[0] == self._training_set_size
+        self._sample_gradient_hook.release_queue()
+        if self._batch_hvp_hook is not None:
+            self._batch_hvp_hook.release_queue()
--- cyy_ml_if/lean_hydra/lean_hydra_hook.py
+++ cyy_ml_if/lean_hydra/lean_hydra_hook.py
@@ -1,64 +1,17 @@
 import json
 import os
 
-import torch
-from cyy_naive_lib.log import get_logger
-from cyy_torch_algorithm.computation.sample_gradient.sample_gradient_hook import \
-    SampleGradientHook
-from cyy_torch_toolbox.device import get_device
-from cyy_torch_toolbox.hook import Hook
+from cyy_ml_if.hydra.base_hook import BaseHook
 
 
-class LeanHyDRAHook(Hook):
+class LeanHyDRAHook(BaseHook):
     def __init__(self, test_gradient, **kwargs):
-        super().__init__(stripable=True)
-        self.sample_gradient_hook = SampleGradientHook(**kwargs)
-        self._computed_indices = None
+        super().__init__(**kwargs)
         self.__test_gradient = test_gradient.cpu()
-        self._contributions: torch.Tensor | None = None
-        self._training_set_size = None
         self.sample_gradient_hook.set_result_transform(self._gradient_dot_product)
 
     def _gradient_dot_product(self, result, **kwargs):
         return self.__test_gradient.dot(result.cpu()).item()
-
-    @property
-    def contributions(self):
-        return self._contributions
-
-    def _before_execute(self, **kwargs):
-        if "model_executor" in kwargs:
-            model_executor = kwargs["model_executor"]
-            trainer = model_executor
-            self._training_set_size = len(trainer.dataset_util)
-            device = trainer.device
-        else:
-            self._training_set_size = kwargs["training_set_size"]
-            device = get_device()
-
-        if not self._computed_indices:
-            self._computed_indices = set(range(self._training_set_size))
-        else:
-            get_logger().info("only compute %s indices", len(self._computed_indices))
-        self._contributions = torch.zeros(self._training_set_size).to(
-            device, non_blocking=True
-        )
-
-    def _get_optimizer(self, **kwargs):
-        if "model_executor" in kwargs:
-            trainer = kwargs["model_executor"]
-            return trainer.get_optimizer()
-        return kwargs["optimizer"]
-
-    def _get_model_util(self, **kwargs):
-        if "model_executor" in kwargs:
-            trainer = kwargs["model_executor"]
-            return trainer.model_util
-        return kwargs["model_with_loss"].model_util
-
-    def set_computed_indices(self, computed_indices):
-        self._computed_indices = set(computed_indices)
-        self.sample_gradient_hook.set_computed_indices(computed_indices)
 
     def _after_execute(self, **kwargs):
         assert self._contributions is not None
@@ -76,4 +29,4 @@ class LeanHyDRAHook(Hook):
         ) as f:
             contributions = self._contributions.cpu().tolist()
             json.dump({idx: contributions[idx] for idx in self._computed_indices}, f)
-        self.sample_gradient_hook.release_queue()
+        super()._after_execute(**kwargs)
