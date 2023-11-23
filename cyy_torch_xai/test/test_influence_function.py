2c7ba341b41fdc7b8ba9293641672072776c424c
cyy <cyyever@outlook.com>
2023-11-23 18:06:52 +0800

    Fix code

--- cyy_torch_xai/test/test_influence_function.py
+++ cyy_torch_xai/test/test_influence_function.py
@@ -4,7 +4,7 @@ from cyy_torch_xai.influence_function import compute_influence_function
 
 def test_IF() -> None:
     config = Config(dataset_name="MNIST", model_name="LeNet5")
-    config.hyper_parameter_config.epoch = 2
+    config.hyper_parameter_config.epoch = 10
     trainer = config.create_trainer()
     trainer.train()
     compute_influence_function(trainer, computed_indices=[1, 2])
