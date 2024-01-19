58bc622c07e370c386e973ae8f41ec1cb14dac9b
cyy <cyyever@outlook.com>
2024-01-19 23:02:36 +0800

    Fix code

--- cyy_torch_xai/tracin/base_hook.py
+++ cyy_torch_xai/tracin/base_hook.py
@@ -1,21 +1,25 @@
+from typing import Any, Iterable
+
 import torch
 from cyy_torch_algorithm.computation.sample_gradient.sample_gradient_hook import \
-    get_sample_gradient_dict
+    get_sample_gradients
+from cyy_torch_toolbox import IterationUnit, MachineLearningPhase
 from cyy_torch_toolbox.hook import Hook
-from cyy_torch_toolbox.ml_type import MachineLearningPhase
 
 
 class TracInBaseHook(Hook):
     def __init__(
-        self, test_sample_indices: set | None = None, check_point_gap: int | None = None
-    ):
+        self,
+        test_sample_indices: Iterable[int] | None = None,
+        check_point_gap: tuple[int, IterationUnit] | None = None,
+    ) -> None:
         super().__init__(stripable=True)
 
         self.__check_point_gap = check_point_gap
         self.__test_sample_indices = test_sample_indices
         self.__test_grad_dict: dict = {}
         self._influence_values: dict = {}
-        self.__batch_num = 0
+        self.__batch_num: int = 0
 
     @property
     def test_grad_dict(self) -> dict:
@@ -25,34 +29,33 @@ class TracInBaseHook(Hook):
     def influence_values(self) -> dict:
         return self._influence_values
 
-    @torch.no_grad()
-    def _before_execute(self, **kwargs):
+    def _before_execute(self, **kwargs: Any) -> None:
         self._influence_values = {}
 
-    def __compute_test_sample_gradients(self, executor):
-        if (
-            self.__batch_num != 0
-            and self.__check_point_gap is not None
-            and self.__batch_num % self.__check_point_gap != 0
-        ):
-            return
+    def __compute_test_sample_gradients(self, executor, **kwargs: Any) -> None:
+        if self.__batch_num != 0 and self.__check_point_gap is not None:
+            gap, unit = self.__check_point_gap
+            match unit:
+                case IterationUnit.Batch:
+                    if self.__batch_num % gap != 0:
+                        return
+                case IterationUnit.Epoch:
+                    if kwargs["epoch"] % gap != 0:
+                        return
+                case _:
+                    raise RuntimeError(unit)
         inferencer = executor.get_inferencer(phase=MachineLearningPhase.Test)
-        inferencer.disable_hook("logger")
-        inferencer.disable_hook("performance_metric")
         if self.__test_sample_indices is None:
             self.__test_grad_dict[-1] = inferencer.get_gradient()
         else:
-
-            def collect_result(res_dict):
-                self.__test_grad_dict |= res_dict
-
-            get_sample_gradient_dict(
-                inferencer=inferencer,
-                computed_indices=self.__test_sample_indices,
-                result_collection_fun=collect_result,
+            self.__test_grad_dict.update(
+                get_sample_gradients(
+                    inferencer=inferencer,
+                    computed_indices=self.__test_sample_indices,
+                )
             )
 
     @torch.no_grad()
-    def _before_batch(self, executor, batch_index, **kwargs):
-        self.__compute_test_sample_gradients(executor=executor)
+    def _before_batch(self, **kwargs) -> None:
+        self.__compute_test_sample_gradients(**kwargs)
         self.__batch_num += 1
--- cyy_torch_xai/tracin/tracin_hook.py
+++ cyy_torch_xai/tracin/tracin_hook.py
@@ -1,31 +1,27 @@
 import json
 import os
+from typing import Any
 
 import torch
-from cyy_torch_xai.tracin.base_hook import TracInBaseHook
 from cyy_naive_lib.log import get_logger
-from cyy_torch_algorithm.computation.sample_gradient.sample_gradient_hook import (
-    SampleGradientHook, dot_product)
-from cyy_torch_toolbox.tensor import tensor_to
+from cyy_torch_algorithm.computation.sample_gradient.sample_gradient_hook import \
+    SampleGradientHook
+from cyy_torch_toolbox.tensor import dot_product
+from cyy_torch_xai.tracin.base_hook import TracInBaseHook
 
 
 class TracInHook(TracInBaseHook):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self._sample_grad_hook: SampleGradientHook = SampleGradientHook()
         self.__tracked_indices: None | set = None
 
     def set_tracked_indices(self, tracked_indices: set) -> None:
         self.__tracked_indices = set(tracked_indices)
+        self._sample_grad_hook.set_computed_indices(self.__tracked_indices)
         get_logger().info("track %s indices", len(self.__tracked_indices))
 
-    @torch.no_grad()
-    def _before_execute(self, executor, **kwargs):
-        super()._before_execute(executor=executor, **kwargs)
-        if self.__tracked_indices is not None:
-            self._sample_grad_hook.set_computed_indices(self.__tracked_indices)
-
-    def _after_batch(self, executor, batch_size, **kwargs):
+    def _after_batch(self, executor, batch_size, **kwargs) -> None:
         trainer = executor
         optimizer = trainer.get_optimizer()
         assert len(optimizer.param_groups) == 1
@@ -41,18 +37,13 @@ class TracInHook(TracInBaseHook):
                 if k2 not in self._influence_values[k]:
                     self._influence_values[k][k2] = 0
                 self._influence_values[k][k2] += (
-                    dot_product(
-                        tensor_to(test_grad, device="cpu"),
-                        tensor_to(sample_grad, device="cpu"),
-                    )
-                    * lr
-                    / batch_size
+                    dot_product(test_grad, sample_grad) * lr / batch_size
                 )
         get_logger().error("before reset")
         self._sample_grad_hook.reset_result()
         get_logger().error("after reset")
 
-    def _after_execute(self, executor, **kwargs):
+    def _after_execute(self, executor, **kwargs: Any) -> None:
         if -1 in self._influence_values:
             assert len(self._influence_values) == 1
             self._influence_values = self._influence_values[-1]
