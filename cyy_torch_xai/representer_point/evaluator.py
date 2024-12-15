389a836d84cc2909308c030446b32d719ba7d5f2
cyy <cyyever@outlook.com>
2024-12-15 17:51:52 +0800

    Fix code

--- cyy_torch_xai/representer_point/evaluator.py
+++ cyy_torch_xai/representer_point/evaluator.py
@@ -3,39 +3,30 @@ from typing import Any
 import torch
 import torch.nn
 import torch.utils.hooks
+from cyy_naive_lib.decorator import Decorator
 from cyy_torch_toolbox import ModelEvaluator, SampleTensors
 
 
-class ModelEvaluatorWrapper:
+class OutputFeatureModelEvaluator(Decorator):
     def __init__(self, evaluator: ModelEvaluator) -> None:
-        self.evaluator: ModelEvaluator = evaluator
+        super().__init__(evaluator)
         self._sample_indices: list = []
-
-    def __call__(self, *args: Any, **kwargs: Any) -> Any:
-        self._sample_indices = kwargs["sample_indices"].tolist()
-        return self.evaluator.__call__(*args, **kwargs)
-
-    def __getattr__(self, name: str) -> Any:
-        if name == "evaluator":
-            raise AttributeError()
-        return getattr(self.evaluator, name)
-
-
-class OutputFeatureModelEvaluator(ModelEvaluatorWrapper):
-    def __init__(self, evaluator: ModelEvaluator) -> None:
-        super().__init__(evaluator=evaluator)
         self.__output_features: SampleTensors = {}
-        self.last_module = self.evaluator.model_util.get_last_underlying_module()
-        assert isinstance(self.last_module, torch.nn.Linear)
+        self.last_module = self.model_util.get_last_underlying_module()
+        assert isinstance(self._decorator_object, torch.nn.Linear)
         self.last_module.register_forward_pre_hook(
             hook=self.__feature_hook_impl, with_kwargs=True
         )
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        self._sample_indices = kwargs["sample_indices"].tolist()
+        return self._decorator_object.__call__(*args, **kwargs)
 
     @property
     def output_features(self) -> SampleTensors:
         return self.__output_features
 
-    def __feature_hook_impl(self, module, *args, **kwargs) -> Any:
+    def __feature_hook_impl(self, module, *args: Any, **kwargs: Any) -> Any:
         input_tensor: torch.Tensor = args[0][0]
         assert input_tensor.shape[0] == len(self._sample_indices)
         self.__output_features |= dict(
@@ -50,20 +41,21 @@ class OutputGradientEvaluator(OutputFeatureModelEvaluator):
         self.last_module.register_forward_hook(
             hook=self.__output_hook_impl, with_kwargs=True
         )
-        self.__accumulated_sample_indices = []
-        self.__layer_output_tensors = []
+        self.__accumulated_sample_indices: list[list] = []
+        self.__layer_output_tensors: list[torch.Tensor] = []
 
     @property
     def activation_gradients(self) -> SampleTensors:
-        res = {}
+        res: SampleTensors = {}
         for a, b in zip(
             self.__accumulated_sample_indices, self.__layer_output_tensors, strict=False
         ):
+            assert b.grad is not None
             assert len(a) == b.grad.shape[0]
             res |= dict(zip(a, b.grad, strict=False))
         return res
 
-    def __output_hook_impl(self, module, *args, **kwargs) -> Any:
+    def __output_hook_impl(self, module, *args: Any, **kwargs: Any) -> Any:
         output_tensor: torch.Tensor = args[0][0]
         assert output_tensor.shape[0] == len(self._sample_indices)
         assert output_tensor.grad is None
@@ -90,7 +82,7 @@ class OutputModelEvaluator(OutputFeatureModelEvaluator):
     def output_tensors(self) -> SampleTensors:
         return self.__output_tensors
 
-    def __output_hook_impl(self, module, *args, **kwargs) -> Any:
+    def __output_hook_impl(self, module, *args: Any, **kwargs: Any) -> Any:
         input_tensor: torch.Tensor = args[0][0]
         assert input_tensor.shape[0] == len(self._sample_indices)
         self.__output_tensors |= dict(
