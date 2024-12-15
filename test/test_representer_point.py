358f5985a16ea6a422ddf525b87e2c30511b96c1
cyy <cyyever@outlook.com>
2024-12-15 19:09:05 +0800

    Fix code

--- test/test_representer_point.py
+++ test/test_representer_point.py
@@ -6,18 +6,17 @@ from cyy_torch_xai.representer_point import compute_representer_point_values
 has_cyy_torch_vision: bool = importlib.util.find_spec("cyy_torch_vision") is not None
 
 
-if has_cyy_torch_vision:
-
-    def test_representer_point() -> None:
+def test_representer_point() -> None:
+    if not has_cyy_torch_vision:
         return
-        import cyy_torch_vision  # noqa: F401
+    import cyy_torch_vision  # noqa: F401
 
-        config = Config(dataset_name="MNIST", model_name="LeNet5")
-        config.hyper_parameter_config.epoch = 1
-        config.hyper_parameter_config.learning_rate = 0.01
-        trainer = config.create_trainer()
-        trainer.train()
-        contribution = compute_representer_point_values(
-            trainer=trainer, test_indices=[1, 2], training_indices=[1, 2, 3, 4, 5]
-        )
-        print(contribution.normalized_values)
+    config = Config(dataset_name="MNIST", model_name="LeNet5")
+    config.hyper_parameter_config.epoch = 1
+    config.hyper_parameter_config.learning_rate = 0.01
+    trainer = config.create_trainer()
+    trainer.train()
+    contribution = compute_representer_point_values(
+        trainer=trainer, test_indices=[1, 2], training_indices=[1, 2, 3, 4, 5]
+    )
+    print(contribution.normalized_values)
